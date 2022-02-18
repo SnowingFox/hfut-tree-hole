00252437814346f3eaf9745aa11e1deb48b3dde5
SnowingFox <1503401882@qq.com>
2022-02-18 16:50:12 +0800

    :zap: 设置了跨域

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ import { WrapResponseInterceptor } from './common/interceptors/wrap-response.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
