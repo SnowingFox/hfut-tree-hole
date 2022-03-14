7c7cb862d5eb7837a985cd07fb6c4525a67b70f0
snowingfox <1503401882@qq.com>
2022-03-14 16:05:06 +0800

    :zap: solve some type errors

--- src/service/interceptor.ts
+++ src/service/interceptor.ts
@@ -1,37 +1,33 @@
 import type { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
-import * as _ from 'lodash'
 
 export type ResponseInterceptor = <T extends AxiosResponse>(res: T) => T
 
 export type RequestInterceptor = <T extends AxiosRequestConfig>(config: T) => T
 
 export interface Interceptors {
-  request?: RequestInterceptor[] | ResponseInterceptor
+  request?: RequestInterceptor[] | RequestInterceptor
   response?: ResponseInterceptor[] | ResponseInterceptor
 }
 
-export function addInterceptor(instance: AxiosInstance, interceptor: Interceptors = { request: [], response: [] }) {
-  if (!Array.isArray(interceptor.request)) {
-    if (!_.isFunction(interceptor.request)) {
-      interceptor.request = []
-      // TODO solve type error
-      // @ts-expect-error
-      interceptor.request = [interceptor.request]
-    }
+export type KeyofInterceptors = keyof Interceptors
+
+export function addInterceptor(
+  instance: AxiosInstance,
+  interceptor: Interceptors = { request: [], response: [] },
+) {
+  if (!Array.isArray(interceptor.request) && interceptor.request) {
+    interceptor.request = [interceptor.request]
   }
 
-  if (!Array.isArray(interceptor.response)) {
-    if (!_.isFunction(interceptor.response)) {
-      interceptor.response = []
-    }
-    // @ts-expect-error
+  if (!Array.isArray(interceptor.response) && interceptor.response) {
     interceptor.response = [interceptor.response]
   }
 
-  (Object.keys(interceptor) as ['request', 'response']).forEach((item) => {
+  (Object.keys(interceptor) as KeyofInterceptors[]).forEach((item) => {
     (interceptor[item] as Interceptors[typeof item][])!.forEach((interItem) => {
-      // @ts-expect-error
-      instance.interceptors[item].use(interItem as (ResponseInterceptor | RequestInterceptor))
+      instance.interceptors[item].use(
+        interItem as ResponseInterceptor | RequestInterceptor,
+      )
     })
   })
 }
